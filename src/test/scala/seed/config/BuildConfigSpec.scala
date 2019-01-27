351a6deabd9dfeb108e05562a4c1c768044887ac
Tim Nieradzik <tim@sparse.tech>
2019-01-27 14:56:32 +0100

    BuildConfig: Detect target platforms of test module if unspecified
    Currently, the logic in `BuildConfig` only considers the
    platform-specific modules defined on a test module. Change the logic
    to inherit all targets from the parent module.
    
    Change the type of `import` on `Build` to `Path` to avoid the
    duplicated normalisation logic of `fixPath()`.
    
    Also, break `load()` on `Build` apart into separate functions to
    allow for better testing.

--- src/test/scala/seed/config/BuildConfigSpec.scala
+++ src/test/scala/seed/config/BuildConfigSpec.scala
@@ -6,6 +6,9 @@ import minitest.SimpleTestSuite
 import java.nio.file.Paths
 
 import org.apache.commons.io.FileUtils
+import seed.model.Build
+import seed.model.Build.Project
+import seed.model.Platform.{JVM, JavaScript}
 
 object BuildConfigSpec extends SimpleTestSuite {
   test("Resolve absolute project path") {
@@ -34,5 +37,37 @@ object BuildConfigSpec extends SimpleTestSuite {
 
     val (projectPath, _) = BuildConfig.load(Paths.get("test/a.toml"))
     assertEquals(projectPath, Paths.get("test"))
+  }
+
+  test("Set target platforms on test modules") {
+    val toml = """
+      |[project]
+      |scalaVersion      = "2.12.4-bin-typelevel-4"
+      |scalaJsVersion    = "0.6.26"
+      |scalaOrganisation = "org.typelevel"
+      |testFrameworks    = ["minitest.runner.Framework"]
+      |
+      |[module.example]
+      |root       = "shared"
+      |sources    = ["shared/src"]
+      |targets    = ["js", "jvm"]
+      |
+      |[module.example.test]
+      |sources   = ["shared/test"]
+      |scalaDeps = [
+      |  ["io.monix", "minitest", "2.3.2"]
+      |]
+      |
+      |[module.example.test.js]
+      |sources = ["js/test"]
+    """.stripMargin
+
+    val buildRaw = BuildConfig.parseToml(Paths.get("."))(toml)
+    val build = BuildConfig.processBuild(buildRaw.right.get, _ =>
+      Build(project = Project(scalaVersion = "2.12.8"), module = Map()))
+
+    assertEquals(
+      build.module("example").test.get.targets,
+      List(JavaScript, JVM))
   }
 }
