b767e3a63672ca29b22ae3b208570bd281095625
Tim Nieradzik <tim@sparse.tech>
2019-07-12 15:55:16 +0300

    Build: Fix non-deterministic build failures

--- build.sh
+++ build.sh
@@ -3,26 +3,19 @@ set -x
 curl -L -o csbt https://github.com/coursier/sbt-launcher/releases/download/v1.2.14/csbt
 chmod +x csbt
 export COURSIER_SBT_LAUNCHER_ADD_PLUGIN=true
-./csbt compile || \
-	 ./csbt compile || \
-	 ./csbt compile || \
-	 ./csbt compile || \
-	 ./csbt compile || \
-	 ./csbt compile || \
-	 ./csbt compile || \
-	 ./csbt compile || \
-	 ./csbt compile || \
-	 ./csbt compile || \
-	 ./csbt compile || \
-	 ./csbt compile || \
-	 ./csbt compile || \
-	 ./csbt compile || \
-	 ./csbt compile || \
-	 ./csbt compile || \
-	 ./csbt compile || \
-	 ./csbt compile || \
-	 ./csbt compile || \
-	 ./csbt compile || \
-	 ./csbt compile || \
-	 ./csbt compile || \
-	 ./csbt compile
+echo "compile
+compile
+compile
+compile
+compile
+compile
+compile
+compile
+compile
+compile
+compile
+compile
+compile
+compile
+compile
+exit" | ./csbt
