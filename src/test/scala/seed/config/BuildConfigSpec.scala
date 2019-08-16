tindzk/seed

commit 51a27f5dd907ad302594702bd4c57e9a07a6cfcd

Inherit settings when loading build file
Centralise the logic for inheriting settings from the project
section and base modules. The inheritance step is now performed when
loading the build file. As a consequence, many functions do not need
to take a reference to the project definition or parent modules
anymore. This greatly simplifies the code for generating Bloop/IDEA
configurations, and makes the logic less error-prone.

The build file's project section is now entirely optional and its
values can be set on modules instead. Now, Scala options can be
extended in platform modules, for example to set additional flags
required by the Scala.js compiler such as
`-P:scalajs:sjsDefinedByDefault`.

Furthermore, every module is compiled with the Scala version and the
options it was defined with. Therefore, it is not possible to depend
on modules anymore which have an incompatible Scala version. For
example, including a 2.12 module in a 2.13 module will trigger an
error.

Closes #18.

src/main/scala/seed/artefact/ArtefactResolution.scala
@@ -5,11 +5,12 @@ import java.nio.file.Path
 import seed.Cli.PackageConfig
 import MavenCentral.{CompilerVersion, PlatformVersion}
 import seed.cli.util.Ansi
-import seed.model.Build.{Dep, JavaDep, Module, ScalaDep}
+import seed.model.Build.{Dep, JavaDep, Module, Resolvers, ScalaDep}
 import seed.model.Platform.{JVM, JavaScript, Native}
-import seed.model.{Artefact, Build, Platform, Resolution}
+import seed.model.{Artefact, Platform, Resolution}
 import seed.Log
 import seed.config.BuildConfig
+import seed.config.BuildConfig.Build
 
 object ArtefactResolution {
   def javaDepFromScalaDep(
@@ -52,9 +53,9 @@ object ArtefactResolution {
       version
     )
 
-  def jsPlatformDeps(build: Build, module: Module): Set[JavaDep] = {
-    val scalaVersion   = BuildConfig.scalaVersion(build.project, List(module))
-    val scalaJsVersion = build.project.scalaJsVersion.get
+  def jsPlatformDeps(module: Module): Set[JavaDep] = {
+    val scalaVersion   = module.scalaVersion.get
+    val scalaJsVersion = module.scalaJsVersion.get
 
     Set(
       Artefact.ScalaJsLibrary
@@ -70,9 +71,9 @@ object ArtefactResolution {
     )
   }
 
-  def nativePlatformDeps(build: Build, modules: List[Module]): Set[JavaDep] = {
-    val scalaVersion       = BuildConfig.scalaVersion(build.project, modules)
-    val scalaNativeVersion = build.project.scalaNativeVersion.get
+  def nativePlatformDeps(module: Module): Set[JavaDep] = {
+    val scalaVersion       = module.scalaVersion.get
+    val scalaNativeVersion = module.scalaNativeVersion.get
 
     Set(
       Artefact.ScalaNativeJavalib,
@@ -91,9 +92,9 @@ object ArtefactResolution {
     )
   }
 
-  def nativeLibraryDep(build: Build, modules: List[Module]): JavaDep = {
-    val scalaVersion       = BuildConfig.scalaVersion(build.project, modules)
-    val scalaNativeVersion = build.project.scalaNativeVersion.get
+  def nativeLibraryDep(module: Module): JavaDep = {
+    val scalaVersion       = module.scalaVersion.get
+    val scalaNativeVersion = module.scalaNativeVersion.get
 
     javaDepFromArtefact(
       Artefact.ScalaNativeNativelib,
@@ -104,77 +105,73 @@ object ArtefactResolution {
     )
   }
 
-  def jvmArtefacts(stack: List[Module]): Set[(Platform, Dep)] =
-    stack.flatMap(_.scalaDeps).map(dep => JVM  -> dep).toSet ++
-      stack.flatMap(_.javaDeps).map(dep => JVM -> dep).toSet
+  def jvmArtefacts(module: Module): Set[(Platform, Dep)] =
+    (module.scalaDeps ++ module.javaDeps).map(dep => JVM -> dep).toSet
 
-  def jvmDeps(build: Build, stack: List[Module]): Set[JavaDep] = {
-    val scalaVersion = BuildConfig.scalaVersion(build.project, stack)
-
-    stack
-      .flatMap(_.scalaDeps)
-      .map(dep => javaDepFromScalaDep(dep, JVM, scalaVersion, scalaVersion))
+  def jvmDeps(module: Module): Set[JavaDep] =
+    module.scalaDeps
+      .map(
+        dep =>
+          javaDepFromScalaDep(
+            dep,
+            JVM,
+            module.scalaVersion.get,
+            module.scalaVersion.get
+          )
+      )
       .toSet ++
-      stack.flatMap(_.javaDeps).toSet
-  }
+      module.javaDeps.toSet
 
-  def jsArtefacts(stack: List[Module]): Set[(Platform, Dep)] =
-    stack.flatMap(_.scalaDeps).map(dep => JavaScript -> dep).toSet
+  def jsArtefacts(module: Module): Set[(Platform, Dep)] =
+    module.scalaDeps.map(dep => JavaScript -> dep).toSet
 
-  def jsDeps(build: Build, stack: List[Module]): Set[JavaDep] =
-    build.project.scalaJsVersion match {
-      case None => Set()
-      case Some(scalaJsVersion) =>
-        val scalaVersion = BuildConfig.scalaVersion(build.project, stack)
-        stack
-          .flatMap(_.scalaDeps)
-          .map(
-            dep =>
-              javaDepFromScalaDep(dep, JavaScript, scalaJsVersion, scalaVersion)
+  def jsDeps(module: Module): Set[JavaDep] =
+    module.scalaDeps
+      .map(
+        dep =>
+          javaDepFromScalaDep(
+            dep,
+            JavaScript,
+            module.scalaJsVersion.get,
+            module.scalaVersion.get
           )
-          .toSet
-    }
+      )
+      .toSet
 
-  def nativeArtefacts(stack: List[Module]): Set[(Platform, Dep)] =
-    stack.flatMap(_.scalaDeps).map(dep => Native -> dep).toSet
+  def nativeArtefacts(module: Module): Set[(Platform, Dep)] =
+    module.scalaDeps.map(dep => Native -> dep).toSet
 
-  def nativeDeps(build: Build, stack: List[Module]): Set[JavaDep] =
-    build.project.scalaNativeVersion match {
-      case None => Set()
-      case Some(scalaNativeVersion) =>
-        val scalaVersion = BuildConfig.scalaVersion(build.project, stack)
-        stack
-          .flatMap(_.scalaDeps)
-          .map(
-            dep =>
-              javaDepFromScalaDep(dep, Native, scalaNativeVersion, scalaVersion)
+  def nativeDeps(module: Module): Set[JavaDep] =
+    module.scalaDeps
+      .map(
+        dep =>
+          javaDepFromScalaDep(
+            dep,
+            Native,
+            module.scalaNativeVersion.get,
+            module.scalaVersion.get
           )
-          .toSet
-    }
+      )
+      .toSet
 
-  def compilerDeps(build: Build, module: Module): List[Set[JavaDep]] = {
-    def f(build: Build, module: Module, platform: Platform): Set[JavaDep] = {
-      import build.project.scalaOrganisation
-      val platformModule = BuildConfig.platformModule(module, platform)
+  def compilerDeps(module: Module): List[Set[JavaDep]] = {
+    def f(module: Module, platform: Platform): Set[JavaDep] = {
+      val platformModule = BuildConfig.platformModule(module, platform).get
 
-      val platformVer = BuildConfig.platformVersion(build, module, platform)
-      val compilerVer =
-        BuildConfig.scalaVersion(build.project, platformModule.toList :+ module)
+      val platformVer  = BuildConfig.platformVersion(platformModule, platform)
+      val compilerVer  = platformModule.scalaVersion.get
+      val organisation = platformModule.scalaOrganisation.get
 
       val scalaDeps = Set(
-        Artefact.scalaCompiler(scalaOrganisation) -> compilerVer,
-        Artefact.scalaLibrary(scalaOrganisation)  -> compilerVer,
-        Artefact.scalaReflect(scalaOrganisation)  -> compilerVer
+        Artefact.scalaCompiler(organisation) -> compilerVer,
+        Artefact.scalaLibrary(organisation)  -> compilerVer,
+        Artefact.scalaReflect(organisation)  -> compilerVer
       ) ++ (
         if (platform == Platform.Native)
           Set(Artefact.ScalaNativePlugin -> platformVer)
         else if (platform == Platform.JavaScript)
           Set(Artefact.ScalaJsCompiler -> platformVer)
         else Set()
-      )
-
-      val dependencies = mergeDeps[ScalaDep](
-        module.compilerDeps ++ platformModule.toList.flatMap(_.compilerDeps)
       )
 
       scalaDeps.map {
@@ -186,110 +183,51 @@ object ArtefactResolution {
             platformVer,
             compilerVer
           )
-      } ++ dependencies.map(
+      } ++ platformModule.compilerDeps.map(
         dep => javaDepFromScalaDep(dep, platform, platformVer, compilerVer)
       )
     }
 
-    module.targets.map(target => f(build, module, target)).filter(_.nonEmpty)
+    module.targets.map(target => f(module, target)).filter(_.nonEmpty)
   }
 
   def allCompilerDeps(build: Build): List[Set[JavaDep]] =
-    build.module.values.toList.flatMap(compilerDeps(build, _)).distinct
+    build.values.toList.flatMap(m => compilerDeps(m.module)).distinct
 
   def platformDeps(build: Build, module: Module): Set[JavaDep] =
     module.targets.toSet[Platform].flatMap { target =>
-      if (target == JavaScript)
-        jsPlatformDeps(build, module.js.getOrElse(Module()))
-      else if (target == Native)
-        nativePlatformDeps(build, module.native.toList)
+      if (target == JavaScript) jsPlatformDeps(module.js.get)
+      else if (target == Native) nativePlatformDeps(module.native.get)
       else Set[JavaDep]()
     }
 
-  def libraryDeps(
-    build: Build,
-    module: Module,
-    platforms: Set[Platform],
-    parent: Module = Module()
-  ): Set[JavaDep] = {
-    val targets = if (module.targets.isEmpty) parent.targets else module.targets
-    targets.toSet[Platform].intersect(platforms).flatMap { target =>
-      // Shared libraries
-      if (target == JVM)
-        jvmDeps(
-          build,
-          module.jvm.toList ++ parent.jvm.toList ++ List(module, parent)
-        )
-      else if (target == JavaScript)
-        jsDeps(
-          build,
-          module.js.toList ++ parent.js.toList ++ List(module, parent)
-        )
-      else
-        nativeDeps(
-          build,
-          module.native.toList ++ parent.native.toList ++ List(module, parent)
-        )
-    } ++
-      (if (!platforms.contains(JVM)) Set()
-       else
-         module.jvm.toSet.flatMap(
-           jvm =>
-             jvmDeps(build, List(jvm, parent.jvm.getOrElse(Module()), module))
-         )) ++
+  def libraryDeps(module: Module, platforms: Set[Platform]): Set[JavaDep] =
+    (if (!platforms.contains(JVM)) Set()
+     else module.jvm.toSet.flatMap(jvmDeps)) ++
       (if (!platforms.contains(JavaScript)) Set()
-       else
-         module.js.toSet.flatMap(
-           js => jsDeps(build, List(js, parent.js.getOrElse(Module()), module))
-         )) ++
+       else module.js.toSet.flatMap(jsDeps)) ++
       (if (!platforms.contains(Native)) Set()
-       else
-         module.native.toSet.flatMap(
-           native =>
-             nativeDeps(
-               build,
-               List(native, parent.native.getOrElse(Module()), module)
-             )
-         )) ++
-      module.test.toSet.flatMap(libraryDeps(build, _, platforms, module))
-  }
+       else module.native.toSet.flatMap(nativeDeps)) ++
+      module.test.toSet.flatMap(libraryDeps(_, platforms))
 
-  def libraryArtefacts(
-    build: Build,
-    module: Module,
-    parent: Module = Module()
-  ): Set[(Platform, Dep)] =
-    module.targets.toSet[Platform].flatMap { target =>
-      if (target == JVM) jvmArtefacts(List(module, parent))
-      else if (target == JavaScript) jsArtefacts(List(module, parent))
-      else nativeArtefacts(List(module, parent))
-    } ++
-      module.jvm.toSet.flatMap(
-        jvm => jvmArtefacts(List(jvm, parent.jvm.getOrElse(Module()), module))
-      ) ++
-      module.js.toSet.flatMap(
-        js => jsArtefacts(List(js, parent.js.getOrElse(Module()), module))
-      ) ++
-      module.native.toSet.flatMap(
-        native =>
-          nativeArtefacts(
-            List(native, parent.native.getOrElse(Module()), module)
-          )
-      ) ++
-      module.test.toSet.flatMap(libraryArtefacts(build, _, module))
+  def libraryArtefacts(module: Module): Set[(Platform, Dep)] =
+    module.jvm.toSet.flatMap(jvmArtefacts) ++
+      module.js.toSet.flatMap(jsArtefacts) ++
+      module.native.toSet.flatMap(nativeArtefacts) ++
+      module.test.toSet.flatMap(libraryArtefacts)
 
   def allPlatformDeps(build: Build): Set[JavaDep] =
-    build.module.values.toSet.flatMap(platformDeps(build, _))
+    build.values.toSet.flatMap(m => platformDeps(build, m.module))
 
   def allLibraryDeps(
     build: Build,
     platforms: Set[Platform] = Set(JVM, JavaScript, Native)
   ): Set[JavaDep] =
-    build.module.values.toSet.flatMap(libraryDeps(build, _, platforms))
+    build.values.toSet.flatMap(m => libraryDeps(m.module, platforms))
 
   def allLibraryArtefacts(build: Build): Map[Platform, Set[Dep]] =
-    build.module.values.toSet
-      .flatMap(libraryArtefacts(build, _))
+    build.values.toSet
+      .flatMap(m => libraryArtefacts(m.module))
       .groupBy(_._1)
       .mapValues(_.map(_._2))
 
@@ -305,6 +243,8 @@ object ArtefactResolution {
     classPath: List[Path],
     optionalArtefacts: Boolean
   ): Resolution.ScalaCompiler = {
+    require(classPath.length == classPath.distinct.length)
+
     val compilerDep = JavaDep(scalaOrganisation, "scala-compiler", scalaVersion)
     val libraryDep  = JavaDep(scalaOrganisation, "scala-library", scalaVersion)
     val reflectDep  = JavaDep(scalaOrganisation, "scala-reflect", scalaVersion)
@@ -340,6 +280,7 @@ object ArtefactResolution {
 
   def resolution(
     seedConfig: seed.model.Config,
+    resolvers: Resolvers,
     build: Build,
     packageConfig: PackageConfig,
     optionalArtefacts: Boolean,
@@ -356,15 +297,15 @@ object ArtefactResolution {
     log.info("Configured resolvers:")
     log.detail("- " + Ansi.italic(resolvedIvyPath.toString) + " (Ivy)")
     log.detail("- " + Ansi.italic(resolvedCachePath.toString) + " (Coursier)")
-    build.resolvers.ivy
+    resolvers.ivy
       .foreach(ivy => log.detail("- " + Ansi.italic(ivy.url) + " (Ivy)"))
-    build.resolvers.maven
+    resolvers.maven
       .foreach(maven => log.detail("- " + Ansi.italic(maven) + " (Maven)"))
 
     def resolve(deps: Set[JavaDep]) =
       Coursier.resolveAndDownload(
         deps,
-        build.resolvers,
+        resolvers,
         resolvedIvyPath,
         resolvedCachePath,
         optionalArtefacts,

commit 455186aac1deccb2c9a1e01c57846238f01fadea

BuildConfig: Fix inheritance of settings on test modules
This fixes several regressions introduced in 51a27f5.

src/main/scala/seed/artefact/ArtefactResolution.scala
@@ -250,7 +250,11 @@ object ArtefactResolution {
     val reflectDep  = JavaDep(scalaOrganisation, "scala-reflect", scalaVersion)
 
     val resolution =
-      resolutionResult.find(r => Coursier.hasDep(r, compilerDep)).get
+      resolutionResult
+        .find(r => Coursier.hasDep(r, compilerDep))
+        .getOrElse(
+          throw new Exception(s"Could not find dependency $compilerDep")
+        )
 
     val compiler =
       Coursier.localArtefacts(resolution, Set(compilerDep), false)

src/main/scala/seed/config/BuildConfig.scala
@@ -93,6 +93,9 @@ object BuildConfig {
     )).toMap
   }
 
+  /** @return Take all settings from `parent` and override them with values
+    *         from `m`
+    */
   def inherit(parent: Module)(m: Module): Module = {
     val inheritTargets = if (m.targets.isEmpty) parent.targets else m.targets
 
@@ -103,9 +106,7 @@ object BuildConfig {
       scalaNativeVersion =
         m.scalaNativeVersion.orElse(parent.scalaNativeVersion),
       scalaOptions = (parent.scalaOptions ++ m.scalaOptions).distinct,
-      scalaOrganisation = m.scalaOrganisation
-        .orElse(parent.scalaOrganisation)
-        .orElse(Some(Organisation.Lightbend.packageName)),
+      scalaOrganisation = m.scalaOrganisation.orElse(parent.scalaOrganisation),
       compilerDeps =
         ArtefactResolution.mergeDeps(parent.compilerDeps ++ m.compilerDeps),
       testFrameworks = (parent.testFrameworks ++ m.testFrameworks).distinct,
@@ -115,6 +116,16 @@ object BuildConfig {
       javaDeps = ArtefactResolution.mergeDeps(parent.javaDeps ++ m.javaDeps)
     )
   }
+
+  def defaultSettings(m: Module): Module =
+    m.copy(
+      scalaOrganisation =
+        m.scalaOrganisation.orElse(Some(Organisation.Lightbend.packageName)),
+      jvm = m.jvm.map(defaultSettings),
+      js = m.js.map(defaultSettings),
+      native = m.native.map(defaultSettings),
+      test = m.test.map(defaultSettings)
+    )
 
   def inheritSettings(parent: Module)(module: Module): Module = {
     val mergedModule = inherit(parent)(module)
@@ -152,38 +163,38 @@ object BuildConfig {
         mainClass = None
       )
 
-    result.copy(
-      test = result.test
-        .map(inheritSettings(stripSettings(result)))
-        .map(
-          t =>
-            t.copy(
-              jvm = t.jvm
-                .orElse(
-                  if (!result.targets.contains(Platform.JVM)) None
-                  else Some(Module())
-                )
-                .map(inherit(stripSettings(result.jvm.getOrElse(Module()))))
-                .map(inherit(t))
-                .map(_.copy(targets = List())),
-              js = t.js
-                .orElse(
-                  if (!result.targets.contains(Platform.JavaScript)) None
-                  else Some(Module())
-                )
-                .map(inherit(stripSettings(result.js.getOrElse(Module()))))
-                .map(inherit(t))
-                .map(_.copy(targets = List())),
-              native = t.native
-                .orElse(
-                  if (!result.targets.contains(Platform.Native)) None
-                  else Some(Module())
-                )
-                .map(inherit(stripSettings(result.native.getOrElse(Module()))))
-                .map(inherit(t))
-                .map(_.copy(targets = List()))
-            )
-        )
+    defaultSettings(
+      result.copy(
+        test = result.test
+          .map(inheritSettings(stripSettings(result)))
+          .map(
+            t =>
+              t.copy(
+                jvm =
+                  (if (!t.targets.contains(Platform.JVM)) None
+                   else Some(result.test.flatMap(_.jvm).getOrElse(Module())))
+                    .map(inherit(result.test.getOrElse(Module())))
+                    .map(inherit(stripSettings(result.jvm.getOrElse(Module()))))
+                    .map(inherit(stripSettings(result)))
+                    .map(_.copy(targets = List())),
+                js = (if (!t.targets.contains(Platform.JavaScript)) None
+                      else Some(result.test.flatMap(_.js).getOrElse(Module())))
+                  .map(inherit(result.test.getOrElse(Module())))
+                  .map(inherit(stripSettings(result.js.getOrElse(Module()))))
+                  .map(inherit(stripSettings(result)))
+                  .map(_.copy(targets = List())),
+                native =
+                  (if (!t.targets.contains(Platform.Native)) None
+                   else Some(result.test.flatMap(_.native).getOrElse(Module())))
+                    .map(inherit(result.test.getOrElse(Module())))
+                    .map(
+                      inherit(stripSettings(result.native.getOrElse(Module())))
+                    )
+                    .map(inherit(stripSettings(result)))
+                    .map(_.copy(targets = List()))
+              )
+          )
+      )
     )
   }
 

src/test/scala/seed/artefact/ArtefactResolutionSpec.scala
@@ -1,9 +1,12 @@
 package seed.artefact
 
+import java.io.File
+import java.nio.charset.Charset
 import java.nio.file.Paths
 
 import minitest.SimpleTestSuite
-import seed.config.BuildConfig
+import org.apache.commons.io.FileUtils
+import seed.config.{BuildConfig, BuildConfigSpec}
 import seed.generation.util.ProjectGeneration
 import seed.model.Build.{JavaDep, Module, ScalaDep, VersionTag}
 import seed.model.Platform.{JVM, JavaScript, Native}
@@ -248,5 +251,17 @@ object ArtefactResolutionSpec extends SimpleTestSuite {
         )
       )
     )
+  }
+
+  test("Resolve correct platform libraries on test module") {
+    // The test module overrides the target platforms. Therefore, no Scala
+    // Native dependencies should be fetched.
+    val path = new File("test", "test-module-dep")
+    val tomlBuild =
+      FileUtils.readFileToString(new File(path, "build.toml"), "UTF-8")
+    val build = BuildConfigSpec.parseBuild(tomlBuild)(_ => "")
+
+    val deps = ArtefactResolution.allLibraryDeps(build)
+    assertEquals(deps, Set(JavaDep("org.scalatest", "scalatest_2.11", "3.0.8")))
   }
 }

src/test/scala/seed/config/BuildConfigSpec.scala
@@ -10,7 +10,7 @@ import seed.{Log, LogLevel}
 import seed.config.util.TomlUtils
 import seed.generation.util.BuildUtil
 import seed.model.Build.{JavaDep, Resolvers, ScalaDep, VersionTag}
-import seed.model.Platform.{JVM, JavaScript}
+import seed.model.Platform.{JVM, JavaScript, Native}
 import BuildUtil.tempPath
 import seed.cli.util.Ansi
 import seed.config.BuildConfig.{Build, Result}
@@ -301,6 +301,211 @@ object BuildConfigSpec extends SimpleTestSuite {
     assert(build("example").module.test.get.scalaDeps.isEmpty)
     assert(build("example").module.test.get.js.get.mainClass.isEmpty)
     assert(build("example").module.test.get.js.get.scalaDeps.isEmpty)
+  }
+
+  test("Override target platforms on test module") {
+    val toml =
+      """
+        |[project]
+        |scalaVersion       = "2.11.11"
+        |scalaJsVersion     = "0.6.28"
+        |scalaNativeVersion = "0.3.7"
+        |
+        |[module.example]
+        |sources = ["src/"]
+        |targets = ["jvm", "js", "native"]
+        |
+        |[module.example.test]
+        |sources = ["src/"]
+        |targets = ["jvm", "js"]
+      """.stripMargin
+
+    val build = parseBuild(toml)(_ => "")
+    assertEquals(build("example").module.targets, List(JVM, JavaScript, Native))
+    assertEquals(
+      build("example").module.test.get.targets,
+      List(JVM, JavaScript)
+    )
+    assertEquals(build("example").module.test.get.native, None)
+  }
+
+  test("Set Scala version on test module") {
+    val toml =
+      """
+        |[project]
+        |scalaVersion       = "2.13.0"
+        |scalaNativeVersion = "0.3.9"
+        |
+        |[module.example]
+        |sources = ["shared/src"]
+        |targets = ["jvm", "native"]
+        |
+        |[module.example.native]
+        |scalaVersion = "2.11.12"
+        |sources      = ["native/src"]
+        |
+        |[module.example.test.native]
+      """.stripMargin
+
+    val build = parseBuild(toml)(_ => "")
+    assertEquals(build("example").module.targets, List(Native, JVM))
+    assertEquals(
+      build("example").module.native.get.scalaVersion,
+      Some("2.11.12")
+    )
+    assertEquals(build("example").module.test.get.targets, List(Native, JVM))
+    assertEquals(
+      build("example").module.test.get.native.get.scalaVersion,
+      Some("2.11.12")
+    )
+    assertEquals(build("example").module.test.get.native.get.targets, List())
+  }
+
+  test("Set Scala version on test module (2)") {
+    val toml =
+      """
+        |[project]
+        |scalaVersion       = "2.13.0"
+        |scalaNativeVersion = "0.3.9"
+        |
+        |[module.example]
+        |sources = ["shared/src"]
+        |targets = ["jvm", "native"]
+        |
+        |[module.example.native]
+        |scalaVersion = "2.11.11"
+        |sources      = ["native/src"]
+        |
+        |[module.example.test.native]
+        |scalaVersion = "2.11.12"
+      """.stripMargin
+
+    val build = parseBuild(toml)(_ => "")
+    assertEquals(build("example").module.targets, List(Native, JVM))
+    assertEquals(
+      build("example").module.native.get.scalaVersion,
+      Some("2.11.11")
+    )
+    assertEquals(build("example").module.test.get.targets, List(Native, JVM))
+    assertEquals(
+      build("example").module.test.get.native.get.scalaVersion,
+      Some("2.11.12")
+    )
+    assertEquals(build("example").module.test.get.native.get.targets, List())
+  }
+
+  test("Set Scala version on test module (3)") {
+    val toml =
+      """
+        |[project]
+        |scalaVersion       = "2.13.0"
+        |scalaNativeVersion = "0.3.9"
+        |
+        |[module.example]
+        |sources = ["shared/src"]
+        |targets = ["jvm", "native"]
+        |
+        |[module.example.native]
+        |scalaVersion = "2.11.11"
+        |sources      = ["native/src"]
+        |
+        |[module.example.test.native]
+        |scalaVersion = "2.11.12"
+      """.stripMargin
+
+    val build = parseBuild(toml)(_ => "")
+    assertEquals(build("example").module.targets, List(Native, JVM))
+    assertEquals(
+      build("example").module.native.get.scalaVersion,
+      Some("2.11.11")
+    )
+    assertEquals(build("example").module.test.get.targets, List(Native, JVM))
+    assertEquals(
+      build("example").module.test.get.native.get.scalaVersion,
+      Some("2.11.12")
+    )
+    assertEquals(build("example").module.test.get.native.get.targets, List())
+
+    assertEquals(
+      build("example").module.scalaOrganisation,
+      Some("org.scala-lang")
+    )
+    assertEquals(
+      build("example").module.test.get.scalaOrganisation,
+      Some("org.scala-lang")
+    )
+    assertEquals(
+      build("example").module.test.get.jvm.get.scalaOrganisation,
+      Some("org.scala-lang")
+    )
+    assertEquals(
+      build("example").module.test.get.native.get.scalaOrganisation,
+      Some("org.scala-lang")
+    )
+  }
+
+  test("Set Scala organisation on test module") {
+    val toml =
+      """
+        |[project]
+        |scalaVersion      = "2.11.11-bin-typelevel-4"
+        |scalaJsVersion    = "0.6.28"
+        |scalaOrganisation = "org.typelevel"
+        |
+        |[module.example]
+        |sources = ["shared/src"]
+        |targets = ["js", "jvm"]
+        |
+        |[module.example.test]
+        |sources = ["shared/test"]
+        |targets = ["js", "jvm"]
+        |
+        |[module.example.test.js]
+        |sources = ["js/src"]
+      """.stripMargin
+
+    val build = parseBuild(toml)(_ => "")
+    assertEquals(
+      build("example").module.scalaOrganisation,
+      Some("org.typelevel")
+    )
+    assertEquals(
+      build("example").module.test.get.scalaOrganisation,
+      Some("org.typelevel")
+    )
+    assertEquals(
+      build("example").module.test.get.js.get.scalaOrganisation,
+      Some("org.typelevel")
+    )
+  }
+
+  test("Set Scala organisation on test module (2)") {
+    val toml =
+      """
+        |[module.example]
+        |scalaVersion      = "2.11.11-bin-typelevel-4"
+        |scalaJsVersion    = "0.6.28"
+        |scalaOrganisation = "org.typelevel"
+        |sources           = ["shared/src"]
+        |targets           = ["js", "jvm"]
+        |
+        |[module.example.test]
+        |sources = ["shared/test"]
+        |targets = ["js", "jvm"]
+        |
+        |[module.example.test.js]
+        |sources = ["js/src"]
+      """.stripMargin
+
+    val build = parseBuild(toml)(_ => "")
+    assertEquals(
+      build("example").module.scalaOrganisation,
+      Some("org.typelevel")
+    )
+    assertEquals(
+      build("example").module.test.get.js.get.scalaOrganisation,
+      Some("org.typelevel")
+    )
   }
 
   test("Parse TOML with full Scala dependency") {

src/test/scala/seed/generation/IdeaSpec.scala
@@ -86,6 +86,11 @@ object IdeaSpec extends SimpleTestSuite {
           ModuleConfig(BuildConfig.inheritSettings(Module())(m), Paths.get("."))
       )
 
+    assertEquals(
+      build("c").module.test.get.jvm.get.sources,
+      List(Paths.get("c/test"))
+    )
+
     val projectPath   = Paths.get(".")
     val outputPath    = Paths.get("/tmp")
     val packageConfig = PackageConfig(false, false, None, None)

test/test-module-dep/build.toml
@@ -1,0 +1,18 @@
+[project]
+scalaVersion       = "2.11.11"
+scalaNativeVersion = "0.3.7"
+testFrameworks     = ["org.scalatest.tools.Framework"]
+
+[module.example]
+sources = ["shared/src"]
+targets = ["jvm", "native"]
+
+[module.example.test]
+sources   = ["shared/test"]
+targets   = ["jvm"]
+scalaDeps = [
+  ["org.scalatest", "scalatest", "3.0.8"]
+]
+
+[module.example.jvm]
+sources = ["jvm/src"]