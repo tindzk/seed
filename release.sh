b88eed055558edce3aafcacc7a215d9acdc5bc37
Tim Nieradzik <tim@sparse.tech>
2020-05-01 20:22:24 +0100

    Release: Push to repository first
    This prevents the `publish_release` pipeline from failing in case
    a tagged commit was already published by `publish_prerelease`.
    
    Also ensure that the correct branch is checked out.

--- release.sh
+++ release.sh
@@ -4,8 +4,11 @@ set -x
 
 version=$1
 
+/usr/bin/git checkout master
+/usr/bin/git pull public master
+/usr/bin/git push public
 /usr/bin/git tag -f $version
 /usr/bin/git push public :refs/tags/$version
-/usr/bin/git push public && /usr/bin/git push public --tags
+/usr/bin/git push public --tags
 
 echo See http://ci.sparse.tech/tindzk/seed for status
